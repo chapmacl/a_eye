150bf1975f01d297cb13774d4a75126b22a227ca
cchapman <clayton.chapman@iais.fraunhofer.de>
2021-11-04 17:10:21 +0100

    Introduction screen added. Main purpose is to get permissions

--- a_eye/lib/main.dart
+++ a_eye/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:a_eye/file_explorer.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 import 'camera_screen.dart';
 
 List<CameraDescription> cameras;
@@ -34,13 +34,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // TODO check if user is new or not, return appropriate home screen
       home: MyHomePage(),
     );
   }
 }
-
-// TODO create introduction class here
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -52,10 +49,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 1;
   PageController _pageController;
+  bool newUser;
 
   @override
   void initState() {
     super.initState();
+    newUser = Settings.getValue('newuser', true);
+    if (newUser) {
+      _currentIndex = 0;
+    }
     _pageController = PageController(initialPage: _currentIndex);
   }
 
@@ -68,19 +70,84 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: PageView(
           controller: _pageController,
           onPageChanged: (index) {
             setState(() => _currentIndex = index);
           },
-          children: [
-            FileScreen(_pageController),
-            CameraScreen(cameras),
-            SettingsArea(),
-          ],
+          children: newUser
+              ? [
+                  Container(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/detect.gif'),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16.0, right: 16.0, bottom: 16.0),
+                        child: Text(
+                          'Welcome to A.Eye. \n\n There are just a few things to do before you\'re ready to go.',
+                          textAlign: TextAlign.center,
+                          style: AppTheme.title,
+                        ),
+                      ),
+                      ElevatedButton(
+                        child: Text('Next'),
+                        onPressed: () {
+                          _pageController.animateToPage(1,
+                              curve: Curves.easeOut,
+                              duration: Duration(milliseconds: 250));
+                        },
+                      )
+                    ],
+                  )),
+                  Container(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/permission.gif'),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16.0, right: 16.0, bottom: 16.0),
+                        child: Text(
+                          'In order to use the app, we need permission for the Camera, and Storage.',
+                          textAlign: TextAlign.center,
+                          style: AppTheme.title,
+                        ),
+                      ),
+                      ElevatedButton(
+                        child: Text('Enable'),
+                        onPressed: () async {
+                          Map<Permission, PermissionStatus> statuses = await [
+                            Permission.camera,
+                            Permission.storage,
+                          ].request();
+                          bool permissions = true;
+                          for (var status in statuses.values) {
+                            if (status != PermissionStatus.granted) {
+                              permissions = false;
+                            }
+                          }
+                          if (permissions) {
+                            await Settings.setValue('newuser', false);
+                            setState(() {
+                              newUser = false;
+                            });
+                          }
+                        },
+                      )
+                    ],
+                  )),
+                ]
+              : [
+                  FileScreen(_pageController),
+                  CameraScreen(cameras),
+                  SettingsArea(),
+                ],
         ),
         bottomNavigationBar: BottomNavyBar(
-          containerHeight: 60,
+          containerHeight: newUser ? 0 : 60,
           backgroundColor: AppTheme.notWhite,
           selectedIndex: _currentIndex,
           onItemSelected: (index) {
