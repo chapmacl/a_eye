424710626a69805ae66a0f84bd963b68ede7e197
chapmacl <chapmacl@outlook.com>
2022-11-23 13:19:09 +0100

    Sign in for iOS working

--- a_eye/lib/backend.dart
+++ a_eye/lib/backend.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
@@ -82,7 +83,7 @@ class Backend {
             fontSize: 16.0);
       }
     }
-    updateFolderCount();
+    if (user != null) updateFolderCount();
     return user;
   }
 
